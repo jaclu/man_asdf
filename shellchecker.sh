d55a3799b3818c9b1dca5f94fc38579308cf95d2
jaclu <jacob.lundqvist@gmail.com>
2022-04-14 12:06:47 +0200

    typo

--- shellchecker.sh
+++ shellchecker.sh
@@ -5,7 +5,7 @@
 #
 #  Part of https://github.com/jaclu/man_asdf
 #
-#  Version: 1.0.0 2022-04-14
+#  Version: 1.0.1 2022-04-14
 #
 #  Does shellcheck on all relevant scripts in this project
 #
@@ -27,7 +27,7 @@ checkables=(
 
 for script in "${checkables[@]}"; do
     # abort as soon as one gives warnings
-    echo "Cecking: $script"
+    echo "Checking: $script"
     shellcheck -x "$script" || exit 1
 
 done
